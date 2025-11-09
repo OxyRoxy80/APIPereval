11133fdbfb0d59201a0986ba2f770fbb53acfb8d
Myshevskaya Oxana <myshevskaya_work@mail.ru>
2025-11-09 19:51:37 +0300

    Add method 'partial_update'

--- APIPereval/pereval_app/views.py
+++ APIPereval/pereval_app/views.py
@@ -1,4 +1,5 @@
 from django.db import DatabaseError
+from django.forms import model_to_dict
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -6,6 +7,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Pereval, Level, Images, Coords, User
 from .serializers import PerevalSerializer, LevelSerializer, ImagesSerializer, CoordsSerializer, \
     UserSerializer
+
 
 class PerevalViewSet(ModelViewSet):
     queryset = Pereval.objects.all()
@@ -41,6 +43,40 @@ class PerevalViewSet(ModelViewSet):
                     "id": None
                 }
             )
+
+    def partial_update(self, request, *args, **kwargs):
+        pereval = self.get_object()
+        serializer = self.get_serializer(pereval, data=request.data, partial=True)
+        if pereval.status != 'new':
+            return Response(
+                {
+                    'state': 0,
+                    'message': "Обновлять можно только запись со статусом 'new'!"
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        pereval_user_dict = model_to_dict(pereval.user)
+        pereval_user_dict.pop('id')
+        if request.data.get('user'):
+            pereval_user_data = request.data.get('user')
+            if pereval_user_dict != pereval_user_data:
+                return Response(
+                    {
+                        'state': 0,
+                        'message': 'Нельзя изменять данные пользователя!'
+                    },
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        serializer.is_valid()
+        serializer.save()
+        return Response(
+            {
+                'state': 1,
+                'message': 'Запись успешно обновлена!'
+            },
+            status=status.HTTP_200_OK
+        )
+
 
 class LevelViewSet(ModelViewSet):
     queryset = Level.objects.all()
