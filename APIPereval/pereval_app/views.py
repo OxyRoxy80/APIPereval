378535bf8aea5fcad49e2fe36833c9dccb2e8b49
Myshevskaya Oxana <myshevskaya_work@mail.ru>
2025-11-09 18:02:37 +0300

    Add views

--- APIPereval/pereval_app/views.py
+++ APIPereval/pereval_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Pereval, Level, Images, Coords, User
+from .serializers import PerevalSerializer, LevelSerializer, ImagesSerializer, CoordsSerializer, \
+    UserSerializer
+
+
+class PerevalViewSet(ModelViewSet):
+    queryset = Pereval.objects.all()
+    serializer_class = PerevalSerializer
+
+class LevelViewSet(ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+class ImageViewSet(ModelViewSet):
+    queryset = Images.objects.all()
+    serializer_class = ImagesSerializer
+
+class CoordsViewSet(ModelViewSet):
+    queryset = Coords.objects.all()
+    serializer_class = CoordsSerializer
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
