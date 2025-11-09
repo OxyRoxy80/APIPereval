1d75d595dc6cd07e83c21d2362175f61db530f39
Myshevskaya Oxana <myshevskaya_work@mail.ru>
2025-11-09 17:52:55 +0300

    Add serializers

---
+++ APIPereval/pereval_app/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+
+from drf_writable_nested.serializers import WritableNestedModelSerializer
+
+from pereval_app.models import Coords, User, Level, Images, Pereval
+
+
+class UserSerializer(WritableNestedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'fam', 'name', 'otc', 'phone')
+
+
+class CoordsSerializer(WritableNestedModelSerializer):
+    class Meta:
+        model = Coords
+        fields = ('latitude', 'longitude', 'height')
+
+
+class LevelSerializer(WritableNestedModelSerializer):
+    class Meta:
+        model = Level
+        fields = ('winter', 'summer', 'autumn', 'spring')
+
+
+class ImagesSerializer(WritableNestedModelSerializer):
+    class Meta:
+        model = Images
+        fields = ('data', 'title')
+
+
+class PerevalSerializer(WritableNestedModelSerializer):
+    user = UserSerializer()
+    coords = CoordsSerializer()
+    level = LevelSerializer()
+    images = ImagesSerializer(many=True)
+    status = serializers.CharField(read_only=True)
+    class Meta:
+        model = Pereval
+        fields = (
+            'beauty_title', 'title', 'other_titles', 'connect', 'add_time',
+            'user', 'coords', 'level', 'status', 'images'
+        )
